a8bc632bbad646a219e3586cce74135602e60477
Robert <robert@pop-os.localdomain>
2023-09-05 01:14:00 -0700

    Continuing to build out character creator

--- dnd_character_creator.py
+++ dnd_character_creator.py
@@ -1,18 +1,31 @@
 import dnd_dice
 import pandas as pd
 import numpy as np
+import os
+import time
 
 class Character():
     """Create a class to store the character info"""
     def __init__(self):
         print('Welcome to the character creator!')
+        time.sleep(1)
+        
         self.set_race()
+        time.sleep(1)
+        
         self.set_subrace()
+        time.sleep(1)
+
+        self.set_race_stats()
 
     def set_race(self):
+        # setting the working dir
+        dir, file = os.path.split(__file__)
+        os.chdir(dir)
+        
         global races_df 
         races_df = pd.read_excel(r'./data/character/races.xlsx')
-        print('What race would you like your charcter to be?')
+        print('\nWhat race would you like your charcter to be?')
         
         for counter, race in enumerate(races_df['RACE'].unique(), start=1):
             print(f'\t{counter}. {race}')
@@ -62,9 +75,49 @@ class Character():
         subrace = subraces[subrace_number - 1]
         
         self.subrace = subrace
+
+    
+    def set_race_stats(self):
+        """Confirming selection for race and subrace and setting stats based on the race"""
+        race_stats = races_df[(races_df['RACE'] == self.race) & (races_df['SUBRACE'] == self.subrace)].reset_index(drop=True)
+        if self.race != self.subrace:
+            print(f'\nYou have chosen {self.subrace} {self.race} for your character!')
+        else:
+            print(f'\nYou have chosen {self.race} for your character!')
+        
+        stat_names = ['Race',           'Subrace',          'Size',             'Speed',        'Strength',     
+                      'Dexterity',      'Constitution',     'Intellegence',     'Wisdom',       'Charisma',     
+                      'Special',        'Languages',	    'Extra',            'Book'
+                      ]
+        
+        print('\nYour creature\'s stats will be:')
+        for title, col in zip(stat_names, race_stats.columns):
+            if title == 'Size':
+                if race_stats['SIZE'][0] == 'M':
+                    print('\tSize: Medium')
+                elif race_stats['SIZE'][0] == 'S':
+                    print('\tSize: Small')
+                elif race_stats['SIZE'][0] == 'L':
+                    print('\tSize: Large')
+                else:
+                    print(f"\tSize: {race_stats['SIZE']}")
+            elif title in ['Race', 'Subrace', 'Book']:
+                continue
+            elif (~race_stats[col].isna().values.any()):
+                print(f'\t{title}: {race_stats[col][0]}')
+
+        print("\nWould you like to save these settings for your character? (y/n) ", end ='')
+        race_input = input()
+
+        if race_input.lower() == 'y':
+            self.size = race_stats['SIZE'][0]
+            self.speed = race_stats['SPEED'][0]
+            self.strength           
+        
         
 
 
 if __name__ == '__main__':
+    # print(__file__)
     robert = Character()
-    print(robert.race, robert.subrace)
+    
